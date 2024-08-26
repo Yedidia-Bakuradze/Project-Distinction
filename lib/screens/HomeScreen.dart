6a7e22be429490a4f26c0979b700f29804b112a4
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-08-26 19:10:16 +0300

    The implemention of the homescreen has begun

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:project_distinction/data/users_data.dart';
 import 'package:project_distinction/models/student_model.dart';
@@ -23,7 +24,25 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Hello ${_student.firstName}"), //The real username sgou
+        title: Text("Hello ${_student.firstName}"),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 2 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text("Todo"),
+            style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5),
+            )),
+          ),
+        ],
       ),
     );
   }
