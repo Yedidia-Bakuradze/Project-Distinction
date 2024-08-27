d8e5f5b8c704830c8532a7bdebcdf420a476c49f
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-08-27 16:21:16 +0300

    The list of courses has been added to the Student's model.

--- lib/models/student_model.dart
+++ lib/models/student_model.dart
@@ -1,14 +1,17 @@
+import 'package:project_distinction/models/course_model.dart';
 import 'package:uuid/uuid.dart';
 
 class Student {
-  Student({
-    required this.username,
-    required this.password,
-    required this.firstName,
-    required this.lastName,
-    required this.emailAddress,
-    required this.schoolName,
-  }) : studentId = const Uuid().v1(); // Time baed generated random id
+  Student(
+      {required this.username,
+      required this.password,
+      required this.firstName,
+      required this.lastName,
+      required this.emailAddress,
+      required this.schoolName,
+      courses})
+      : studentId = const Uuid().v1(),
+        listOfCourses = courses ?? []; // Time baed generated random id
 
   String studentId;
   String username;
@@ -21,4 +24,6 @@ class Student {
   String schoolName;
 
   int points = 0;
+
+  List<Course> listOfCourses;
 }
