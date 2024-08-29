065b9f29d4f12126fb06e02c9d86c1cb31131a46
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-08-29 12:11:04 +0300

    Course model has been updated (Issue #3)

--- lib/models/course_model.dart
+++ lib/models/course_model.dart
@@ -1,3 +1,8 @@
+import 'dart:ui';
+
+import 'package:project_distinction/models/exam_model.dart';
+import 'package:project_distinction/models/quiz_model.dart';
+import 'package:project_distinction/models/task_model.dart';
 import 'package:uuid/uuid.dart';
 
 class Course {
@@ -10,8 +15,15 @@ class Course {
     required this.assignmentWeight,
     required this.numOfCalculatedQuizzes,
     required this.numOfCalculatedAssignment,
+    required this.tasks,
+    required this.imageUrl,
+    required this.color,
+    required this.exams,
+    required this.quizzes,
+    this.specialExam,
   }) : courseId = const Uuid().v1();
 
+  String imageUrl;
   String title;
   double credits;
   double totalHourCount;
@@ -24,4 +36,11 @@ class Course {
 
   int numOfCalculatedQuizzes;
   int numOfCalculatedAssignment;
+  String? specialExam;
+  Color color;
+
+  //List of tasks related to the course
+  List<Task> tasks;
+  List<Quiz> quizzes;
+  List<Exam> exams;
 }
