26477840b0bc957fd678222fc52ff62e34307dd4
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-08-29 12:02:47 +0300

    Major model has been updated (Issue #5)

--- lib/models/major_model.dart
+++ lib/models/major_model.dart
@@ -1,15 +1,29 @@
+import 'dart:ui';
+
+import 'package:flutter/rendering.dart';
+import 'package:project_distinction/models/course_model.dart';
 import 'package:uuid/uuid.dart';
 
 class Major {
   Major({
     required this.title,
-    required this.numOfCredits,
+    required this.requiredCredits,
     required this.type,
     required this.majorId,
+    required this.requiredCourses,
+    required this.optionalCourses,
+    required this.color,
   }) : majorid = const Uuid().v1();
+
   String majorid;
   String title;
-  int numOfCredits;
+  String imageUrl = "";
+  double requiredCredits;
   String type;
   String majorId;
+
+  List<Course> requiredCourses;
+  List<Course> optionalCourses;
+
+  Color color;
 }
