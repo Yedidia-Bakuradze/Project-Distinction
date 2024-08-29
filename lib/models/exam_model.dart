7afec4a8e983560736e2cc686b66ff61f5502a06
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-08-29 12:37:12 +0300

    Exam model has been updated (Issue #6)

--- lib/models/exam_model.dart
+++ lib/models/exam_model.dart
@@ -3,9 +3,12 @@ import 'package:uuid/uuid.dart';
 class Exam {
   Exam({
     required this.title,
+    required this.term,
+    this.examDate,
   }) : examId = const Uuid().v1();
 
   String title;
+  String term;
   String examId;
   DateTime? examDate;
 }
