95e7f707a0782bda9d852c10151211853ca909ae
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-08-29 12:38:48 +0300

    Quiz model has been updated (Issue #7)

--- lib/models/quiz_model.dart
+++ lib/models/quiz_model.dart
@@ -3,9 +3,11 @@ import 'package:uuid/uuid.dart';
 class Quiz {
   Quiz({
     required this.title,
+    required this.quizNumber,
   }) : quizId = const Uuid().v1();
 
   String title;
   String quizId;
+  int quizNumber;
   DateTime? quizDate;
 }
